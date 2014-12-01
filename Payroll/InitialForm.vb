a282255b8892573061089a3f993c3fe4bfdde600
Lucy Johnson <lucymariej@gmail.com>
2014-11-30 23:03:01 -0600

    Attempting to Activate File Data
    Currently broken on line 34 of InitialForm.vb

--- Payroll/InitialForm.vb
+++ Payroll/InitialForm.vb
@@ -1,4 +1,6 @@
-﻿Public Class InitialForm
+﻿Imports System.IO
+
+Public Class InitialForm
 
     Private Sub btn_Employee_Click(sender As Object, e As EventArgs) Handles btn_Employee.Click
         Dim employeeLaunch As EmployeeClockInMainMenu
@@ -19,4 +21,39 @@
         Me.Close()
     End Sub
 
+    Private Sub InitialForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim spath As String = Directory.GetCurrentDirectory
+
+        ' creates file that will contain employee data if none is found in current directory
+        Dim paramArray1(2) As String
+        paramArray1(1) = spath
+        paramArray1(2) = "EmployeeData"
+        Dim paramArray2(2) As String
+        paramArray2(1) = spath
+        paramArray2(2) = "EmployeeData\Data.csv"
+        If (Not Directory.Exists(Path.Combine(paramArray1))) Then
+            Directory.CreateDirectory(Path.Combine(paramArray1))
+            If (Not File.Exists(Path.Combine(paramArray2))) Then
+                File.Create(Path.Combine(paramArray2))
+                ' initializes the file that is created to hold the four default names and their data
+                My.Computer.FileSystem.WriteAllText("EmployeeData\Data.csv", "Al Pacino,11111,0;", True)
+                My.Computer.FileSystem.WriteAllText("EmployeeData\Data.csv", "James Cameron,22222,0;", True)
+                My.Computer.FileSystem.WriteAllText("EmployeeData\Data.csv", "Martin Scorcese,33333,0;", True)
+                My.Computer.FileSystem.WriteAllText("EmployeeData\Data.csv", "Ridley Scott,44444,0;", True)
+            End If
+        End If
+
+        Dim currentFile(10) As String
+        Dim i As Integer
+        i = 0
+
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser("EmployeeData\Data.csv")
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(";")
+            While Not MyReader.EndOfData
+                currentFile(i) = MyReader.ReadToEnd()
+                i = i + 1
+            End While
+        End Using
+    End Sub
 End Class
